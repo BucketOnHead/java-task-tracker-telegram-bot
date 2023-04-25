194014bc7f90d6286894ab93febb22e67bd4ac9f
IvanMarakanov <marakanovivan@gmail.com>
2023-04-25 16:12:51 +0300

    add: rabbitmq constants

---
+++ dispatcher/src/main/java/com/github/bucketonhead/config/RabbitQueueConfig.java
@@ -0,0 +1,27 @@
+package com.github.bucketonhead.config;
+
+import com.github.bucketonhead.constants.RabbitQueue;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitQueueConfig {
+
+    @Bean
+    public MessageConverter jsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public Queue textMessageQueue() {
+        return new Queue(RabbitQueue.TEXT_MESSAGE_UPDATE);
+    }
+
+    @Bean
+    public Queue answerMessageQueue() {
+        return new Queue(RabbitQueue.ANSWER_MESSAGE);
+    }
+}
