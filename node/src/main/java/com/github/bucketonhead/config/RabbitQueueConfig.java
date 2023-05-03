bd68e96cf3e5823c227bdd12bed26fddbc862bd9
IvanMarakanov <marakanovivan@gmail.com>
2023-05-03 11:25:17 +0300

    refactor: add node to docker-compose
    move the external environment configuration to docker-compose

--- node/src/main/java/com/github/bucketonhead/config/RabbitQueueConfig.java
+++ node/src/main/java/com/github/bucketonhead/config/RabbitQueueConfig.java
@@ -1,5 +1,7 @@
 package com.github.bucketonhead.config;
 
+import com.github.bucketonhead.constants.RabbitQueue;
+import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
@@ -11,5 +13,15 @@ public class RabbitQueueConfig {
     @Bean
     public MessageConverter jsonMessageConverter() {
         return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public Queue textMessageQueue() {
+        return new Queue(RabbitQueue.TEXT_MESSAGE_UPDATE);
+    }
+
+    @Bean
+    public Queue answerMessageQueue() {
+        return new Queue(RabbitQueue.ANSWER_MESSAGE);
     }
 }
