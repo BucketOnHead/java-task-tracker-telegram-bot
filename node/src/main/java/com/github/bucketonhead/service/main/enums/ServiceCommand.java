5fdeec7f97a4b8b9fe9c37ba5f9b82b34ff95a56
IvanMarakanov <marakanovivan@gmail.com>
2023-05-04 07:45:33 +0300

    refactor: changed implementation
    The implementation has been changed, the functionality has remained the same, the concept of prefix command has been added, as well as a small naming of variables.

--- node/src/main/java/com/github/bucketonhead/service/main/enums/ServiceCommand.java
+++ node/src/main/java/com/github/bucketonhead/service/main/enums/ServiceCommand.java
@@ -1,19 +1,20 @@
 package com.github.bucketonhead.service.main.enums;
 
 public enum ServiceCommand {
-    CANCEL("/cancel"),
-    HELP("/help"),
-    START("/start");
-    private final String value;
+    CANCEL("cancel"),
+    HELP("help"),
+    START("start");
+    public static final String PREFIX = "/";
+    private final String command;
 
-    ServiceCommand(String value) {
-        this.value = value;
+    ServiceCommand(String command) {
+        this.command = PREFIX + command;
     }
 
-    public static ServiceCommand fromValue(String s) {
+    public static ServiceCommand fromValue(String value) {
         ServiceCommand serviceCommand = null;
         for (ServiceCommand cmd : ServiceCommand.values()) {
-            if (cmd.value.equals(s)) {
+            if (cmd.command.equals(value)) {
                 serviceCommand = cmd;
                 break;
             }
@@ -24,6 +25,6 @@ public enum ServiceCommand {
 
     @Override
     public String toString() {
-        return value;
+        return command;
     }
 }
