786717db628f92dba5ade6acf6f1a97b0985e4b6
IvanMarakanov <marakanovivan@gmail.com>
2023-05-27 18:53:44 +0300

    refactor: move MessageSenderImpl to package impl

--- node/src/main/java/com/github/bucketonhead/service/sender/impl/MessageSenderImpl.java
+++ node/src/main/java/com/github/bucketonhead/service/sender/impl/MessageSenderImpl.java
@@ -1,6 +1,7 @@
-package com.github.bucketonhead.service.sender;
+package com.github.bucketonhead.service.sender.impl;
 
 import com.github.bucketonhead.service.rabbitmq.ProducerService;
+import com.github.bucketonhead.service.sender.MessageSender;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
